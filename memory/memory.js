d9324edad95e999163b7b623917ae48dcd5554b1
Donald Macritchie <donald.macritchie@outlook.com>
2024-05-24 18:34:24 +0100

    begun memory game

--- memory/memory.js
+++ memory/memory.js
@@ -4,29 +4,29 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-  this.classList.add('flip');
+    this.classList.add('flip');
 
-  if (!hasFlippedCard) {
-    // first click
-    hasFlippedCard = true;
-    firstCard = this;
-  } else {
-    // second click
-    hasFlippedCard = false;
-    secondCard = this;
-
-    if (firstCard.dataset.logo === secondCard.dataset.logo) {
-      // it's a match!
-      firstCard.removeEventListener('click', flipCard);
-      secondCard.removeEventListener('click', flipCard);
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
     } else {
-      // not a match
-      setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-      }, 1500);
+        // second card
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // do the cards match?
+        if (firstCard.dataset.logo === secondCard.dataset.logo){
+            // its a match
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            // Not a match
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+        }, 1500);
     }
-  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
