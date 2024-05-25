0cc757940f8c903e72b1a0ecf9526fda1ec1592d
Tharindu Nadeeshan <hpatnadeeshan@gmail.com>
2024-05-25 10:42:27 +0100

    added words and hints as a map - added functionality to select random word for each game

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -4,7 +4,35 @@ var width = 5;
 var row = 0; 
 var col = 0; 
 var gameOver = false;
-var word = "FFORC";
+// var word = "FFORC";
+
+const words = [
+    { term: "FORCE", hint: "The mystical energy field that binds the galaxy together." },
+    { term: "SITHS", hint: "Dark side practitioners who oppose the Jedi." },
+    { term: "DROID", hint: "Mechanical beings programmed to serve various purposes." },
+    { term: "JABBA", hint: "Infamous Hutt crime lord." },
+    { term: "PADME", hint: "Former queen and senator of Naboo." },
+    { term: "REBEL", hint: "Group opposing the tyranny of the Galactic Empire." },
+    { term: "EWOKS", hint: "Small, furry inhabitants of the forest moon of Endor." },
+    { term: "LEIAS", hint: "Princess and later General of the Rebel Alliance." },
+    { term: "CLONE", hint: "Genetically identical soldiers created for the Grand Army of the Republic." },
+    { term: "HOLDO", hint: "Vice Admiral in the Resistance." },
+    { term: "BINKS", hint: "Gungan from the planet Naboo." },
+    { term: "ANAKI", hint: "Jedi Knight who fell to the dark side and became Darth Vader." },
+    { term: "WEDGE", hint: "Skilled X-wing pilot and member of Rogue Squadron." },
+    { term: "JANGO", hint: "Bounty hunter and template for the clone army." },
+    { term: "POE", hint: "Skilled pilot and commander in the Resistance." },
+    { term: "SABRE", hint: "Elegant weapon used by Jedi and Sith." },
+    { term: "TROOP", hint: "Group of soldiers or fighters." },
+    { term: "WAMPA", hint: "Large, predatory creature native to the ice planet Hoth." },
+    { term: "BATUU", hint: "Remote outpost planet on the edge of the galaxy." },
+    { term: "HONDO", hint: "Weequay pirate and smuggler." }
+];
+
+var selectedWordObj = words[Math.floor(Math.random() * words.length)];
+var word = selectedWordObj.term;
+var hint = selectedWordObj.hint;
+console.log(word);
 
 window.onload = function(){
     intialize();
