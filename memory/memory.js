f96cff0027304f84f1e783ff7753455b6ce9bb35
Donald Macritchie <donald.macritchie@outlook.com>
2024-05-25 10:38:35 +0100

    added shuffle functionality to memory.js

--- memory/memory.js
+++ memory/memory.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
@@ -15,7 +19,6 @@ function flipCard() {
   }
 
   // second click
-  hasFlippedCard = false;
   secondCard = this;
 
   checkForMatch();
@@ -30,13 +33,33 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
 }
 
 function unflipCards() {
+  lockBoard = true;
+
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
+
+    resetBoard();
   }, 1500);
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+// The cardds will need to be shuffled every time the player starts
+// Brackets round the funtion will immediatley invoke the function
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
